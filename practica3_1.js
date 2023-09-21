6059b87560eecd3dd714a32495ade178f185275e
Carmen Ortega <ortega.139929@e.unavarra.es>
2023-09-21 18:08:16 +0200

    completa primera parte

--- practica3_1.js
+++ practica3_1.js
@@ -1,6 +1,6 @@
 function obtenerDirector() {
     var titulo = document.getElementById("tituloPelicula").value;
-    var peticion = "http://wwww.omdbapi.com/?apikey=b0560da&s=" + titulo;
+    var peticion = `http://www.omdbapi.com/?apikey=b0560da&s=/${titulo}`;
     
     fetch(peticion)
         .then(function(response) {
@@ -13,13 +13,20 @@ function obtenerDirector() {
             // Almacena el JSON de la respuesta en una variable
             var respuestaJson = data;
 
-            // Hacer algo con la respuesta JSON (por ejemplo, mostrarla en la consola)
-            console.log(respuestaJson);
-
             // También puedes acceder a datos específicos, por ejemplo, los resultados
             var resultados = respuestaJson.Search;
             if (resultados) {
-                console.log("Resultados:", resultados);
+                console.log(resultados);
+                var n = resultados.lenght;
+                var cuadroRespuestas = document.createElement("div");
+                var mensaje = `</h2> Todas las películas de ${titulo} </h2>`;
+                mensaje += "<ul>";
+                resultados.forEach(function(resultado) {
+                    mensaje += `<li>${resultado.Title}</br>Año de la película:${resultado.Year}</br></li>`;
+                });
+                mensaje += "</ul>";
+                cuadroRespuestas.innerHTML = mensaje;
+                document.body.appendChild(cuadroRespuestas);
             }
 
         })
