bce541a8fb8daae5f15936987ea66010d1e561aa
Carmen Ortega <ortega.139929@e.unavarra.es>
2023-09-28 16:20:48 +0200

    commit con todos los endpoints definidos

--- practica3_2.js
+++ practica3_2.js
@@ -29,7 +29,7 @@ function saveDatabase(database) {
 app.get('/peliculas/:id', (req, res) => {
   const id = req.params.id;
   const database = loadDatabase();
-  const movie = database.movies.find((m) => m.id === id);
+  const movie = database.peliculas.find((m) => m.id === id);
   if (movie) {
     res.json(movie);
   } else {
@@ -42,7 +42,7 @@ app.post('/peliculas', (req, res) => {
   const { id, nombre, anoPublicacion, actores } = req.body;
   const database = loadDatabase();
   const newMovie = { id, nombre, anoPublicacion, actores };
-  database.movies.push(newMovie);
+  database.peliculas.push(newMovie);
   saveDatabase(database);
   res.json(newMovie);
 });
@@ -51,9 +51,9 @@ app.post('/peliculas', (req, res) => {
 app.delete('/peliculas/:id', (req, res) => {
   const id = req.params.id;
   const database = loadDatabase();
-  const index = database.movies.findIndex((m) => m.id === id);
+  const index = database.peliculas.findIndex((m) => m.id === id);
   if (index !== -1) {
-    database.movies.splice(index, 1);
+    database.peliculas.splice(index, 1);
     saveDatabase(database);
     res.json({ message: 'Película eliminada con éxito' });
   } else {
@@ -66,7 +66,7 @@ app.put('/peliculas/:id', (req, res) => {
   const id = req.params.id;
   const { nombre, anoPublicacion, actores } = req.body;
   const database = loadDatabase();
-  const movie = database.movies.find((m) => m.id === id);
+  const movie = database.peliculas.find((m) => m.id === id);
   if (movie) {
     if (nombre) movie.nombre = nombre;
     if (anoPublicacion) movie.anoPublicacion = anoPublicacion;
@@ -78,7 +78,45 @@ app.put('/peliculas/:id', (req, res) => {
   }
 });
 
-// Más endpoints para acciones relacionadas con actores, como crear, borrar y modificar
+// Endpoint para crear un actor
+app.post('/actores', (req, res) => {
+  const { idA, nombreCompleto, anoNacimiento } = req.body;
+  const database = loadDatabase();
+  const newActor = { idA, nombreCompleto, anoNacimiento };
+  database.actores.push(newActor);
+  saveDatabase(database);
+  res.json(newMovie);
+});
+
+// Endpoint para borrar un actor por ID
+app.delete('/actores/:id', (req, res) => {
+  const id = req.params.id;
+  const database = loadDatabase();
+  const index = database.actores.findIndex((m) => m.id === id);
+  if (index !== -1) {
+    database.actores.splice(index, 1);
+    saveDatabase(database);
+    res.json({ message: 'Actor eliminado con éxito' });
+  } else {
+    res.status(404).json({ error: 'Actor no encontrado' });
+  }
+});
+
+// Endpoint para modificar un actor por ID
+app.put('/actores/:id', (req, res) => {
+  const id = req.params.id;
+  const { nombreCompleto, anoNacimiento} = req.body;
+  const database = loadDatabase();
+  const actor = database.actores.find((m) => m.id === id);
+  if (actor) {
+    if (nombre) actor.nombreCompleto = nombre;
+    if (anoPublicacion) actor.anoNacimiento = anoNacimiento;
+    saveDatabase(database);
+    res.json(actor);
+  } else {
+    res.status(404).json({ error: 'Película no encontrada' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`API escuchando en http://localhost:${port}`);
